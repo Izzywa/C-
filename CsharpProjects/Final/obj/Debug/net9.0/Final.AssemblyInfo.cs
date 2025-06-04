8ce5f49e7ea48a201fe29e6c63bbfc61ba52b9a9
Izzatul Najwa binti Ismail <inbi1@student.london.ac.uk>
2025-06-04 13:28:59 +0800

    add the other files

--- CsharpProjects/Final/obj/Debug/net9.0/Final.AssemblyInfo.cs
+++ CsharpProjects/Final/obj/Debug/net9.0/Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95144f3f61459016e056b71d91d5cb4b8fa2e0fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bacf32dac75b4447964b94d509fa3d6bbf7c830e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
